7eff88b76ceb8d0b238d846c13e1c025555b27f0
BoogieNaipe <142553202+BoogieNaipe@users.noreply.github.com>
2023-12-02 17:54:50 -0300

    Main page (#1)
    * new file:   .gitignore
    	new file:   WalkMyPet.zip
    	new file:   WalkMyPet/Cadastro/index.html
    	new file:   WalkMyPet/Cadastro/style.css
    	new file:   WalkMyPet/Landpage/.vscode/launch.json
    	new file:   WalkMyPet/Landpage/img/Arrow.png
    	new file:   WalkMyPet/Landpage/img/Arrow.svg
    	new file:   WalkMyPet/Landpage/img/Coleira dog.png
    	new file:   WalkMyPet/Landpage/img/Wlakmypet.png
    	new file:   WalkMyPet/Landpage/index.html
    	new file:   WalkMyPet/Landpage/style.css
    	new file:   WalkMyPet/MainPage/img/Wlakmypet.png
    	new file:   WalkMyPet/MainPage/index.html
    	new file:   WalkMyPet/MainPage/script.js
    	new file:   WalkMyPet/MainPage/style.css
    	new file:   "WalkMyPet/P\303\241gina Principal/P\303\241ginaPrincipal.html"
    	new file:   "WalkMyPet/P\303\241gina Principal/img/Wlakmypet.png"
    	new file:   "WalkMyPet/P\303\241gina Principal/img/_82a9d276-7e9e-412d-89ce-5d0342a9a715.png"
    	new file:   "WalkMyPet/P\303\241gina Principal/scripts.js"
    	new file:   "WalkMyPet/P\303\241gina Principal/style.css"
    
    * modified:   README.md

---
+++ WalkMyPet/MainPage/script.js
@@ -0,0 +1,116 @@
+var myButton = document.getElementById("myButton");
+var logo = document.getElementById("logo");
+var fullScreenMenu = document.getElementById("fullScreenMenu");
+var backButton = document.querySelectorAll('.back-btn');
+var animationInProgress = false;
+
+if(myButton && logo && fullScreenMenu) {
+  myButton.addEventListener("mousedown", function() {
+    logo.style.transform = "scale(1.2)";
+  });
+
+  myButton.addEventListener("mouseup", function() {
+    logo.style.transform = "scale(1)";
+  });
+
+  myButton.addEventListener("touchstart", function() {
+    logo.style.transform = "scale(1.2)";
+  });
+
+  myButton.addEventListener("touchend", function() {
+    logo.style.transform = "scale(1)";
+  });
+
+  myButton.addEventListener("click", function() {
+    if (animationInProgress) {
+      return;
+    }
+    animationInProgress = true;
+    if (fullScreenMenu.style.display === "none") {
+      fullScreenMenu.style.display = "block";
+      anime({
+        targets: fullScreenMenu,
+        width: ['0%', '100%'],
+        height: ['0%', '100%'],
+        borderRadius: ['50%', '0%'],
+        easing: 'easeInOutQuad',
+        duration: 500,
+        complete: function() {
+          animationInProgress = false;
+        }
+      });
+    } else {
+      anime({
+        targets: fullScreenMenu,
+        width: ['100%', '0%'],
+        height: ['100%', '0%'],
+        borderRadius: ['0%', '50%'],
+        easing: 'easeInOutQuad',
+        duration: 500,
+        complete: function() {
+          fullScreenMenu.style.display = "none";
+          animationInProgress = false;
+        }
+      });
+    }
+  });
+}
+
+if(backButton && fullScreenMenu) {
+  backButton.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      if (animationInProgress) {
+        return;
+      }
+      animationInProgress = true;
+      var parentMenu = this.parentNode;
+      anime({
+        targets: parentMenu,
+        width: ['100%', '0%'],
+        height: ['100%', '0%'],
+        borderRadius: ['0%', '50%'],
+        easing: 'easeInOutQuad',
+        duration: 500,
+        complete: function() {
+          parentMenu.style.display = "none";
+          animationInProgress = false;
+        }
+      });
+    });
+  });
+}
+
+document.getElementById('map-overlay').addEventListener('click', function() {
+  var mapFullScreenMenu = document.getElementById("mapFullScreenMenu");
+  if (animationInProgress) {
+    return;
+  }
+  animationInProgress = true;
+  if (mapFullScreenMenu.style.display === "none") {
+    mapFullScreenMenu.style.display = "block";
+    anime({
+      targets: mapFullScreenMenu,
+      width: ['0%', '100%'],
+        height: ['0%', '100%'],
+        borderRadius: ['50%', '0%'],
+        easing: 'easeInOutQuad',
+        duration: 1000,
+        complete: function() {
+          animationInProgress = false;
+        }
+    });
+  } else {
+    anime({
+      targets: mapFullScreenMenu,
+      width: ['100%', '0%'],
+      height: ['100%', '0%'],
+      borderRadius: ['0%', '50%'],
+      easing: 'easeInOutQuad',
+      duration: 1000,
+      complete: function() {
+        mapFullScreenMenu.style.display = "none";
+        animationInProgress = false;
+      }
+    });
+  }
+});
