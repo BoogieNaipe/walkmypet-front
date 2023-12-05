98ab79eef3c605d2143df39ea7539a71c6eb745a
BoogieNaipe <marcelovldgomes@outlook.com>
2023-12-05 20:02:45 -0300

    Final

--- WalkMyPet/Página Principal/scripts.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.login-btn').addEventListener('click', function() {
-    alert('Clique no botão Login!');
-});
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
